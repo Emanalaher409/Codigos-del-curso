845a51a7d6a4daa2f1b040e734043fb8d97455d3
Emanalaher409 <108510650+Emanalaher409@users.noreply.github.com>
2022-07-10 19:35:38 -0500

    Add files via upload

---
+++ Herencia/src/domain/Empleado.java
@@ -0,0 +1,53 @@
+
+package domain;
+
+   
+ 
+public class Empleado extends Persona{
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleado;
+    
+    public Empleado (){
+        
+        this.idEmpleado = ++Empleado.contadorEmpleado;
+    }
+
+    public Empleado(String nombre, double sueldo) {
+      
+       this();
+       this.nombre = nombre;
+       this.sueldo = sueldo;
+       
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+   
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{");
+        sb.append("idEmpleado=").append(idEmpleado);
+        sb.append(", sueldo=").append(sueldo);
+        sb.append('}');
+        sb.append(", Persona[=").append(super.toString()).append("]");
+        return sb.toString();
+    }
+
+ 
+    
+    
+    
+}
