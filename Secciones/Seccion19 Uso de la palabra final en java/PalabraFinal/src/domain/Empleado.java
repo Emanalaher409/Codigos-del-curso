4d337eb946c31af01483725b3afcfddbd462bd70
Emanalaher409 <108510650+Emanalaher409@users.noreply.github.com>
2022-07-10 19:52:09 -0500

    Add files via upload

---
+++ Secciones/Seccion19 Uso de la palabra final en java/PalabraFinal/src/domain/Empleado.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+public class Empleado {
+//    @Override
+//  public void imprimir(){
+//        System.out.println("Metodo imprimir desde clase hija");
+//    }
+}
---
+++ Secciones/Seccion19 Uso de la palabra final en java/PalabraFinal/src/domain/Persona.java
@@ -0,0 +1,11 @@
+
+package domain;
+
+
+public final class Persona {
+    public final static int CONSTANTE = 1;
+    public final void imprimir(){
+        System.out.println("Metodo imprimir");
+        System.out.println(CONSTANTE);
+    }
+}
